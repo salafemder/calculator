7bdcbd66ade869caa74c4a6db654c937add52f8c
Salamander <172087794+Salamander2281@users.noreply.github.com>
2025-06-04 20:39:07 -0400

    Add functional buttons with data reflected in the display window of calculator

--- calculator-1.0.js
+++ calculator-1.0.js
@@ -30,3 +30,18 @@ const operate = (operator, numb1, numb2) => {
 let firstNumb = undefined;
 let secondNumb = undefined;
 let operator = undefined;
+
+const btns = document.querySelector('.buttonContainer');
+    btns.addEventListener('click', (event) => {
+        let calcInput = event.target.textContent
+        let calcDisplay = document.querySelector('.display')
+        calcDisplay.textContent = calcInput
+    });
+    
+
+    //display text content should allow for more numbers to be concated to it
+    //if user presses operator sign, firstNumb variable to store display content
+    //operator variable to store pressed operator sign when firstNumb is stored
+    //repeat above steps, storing in secondNumb when = sign is pressed
+    //return appropriate function with value converted to string data type
+    //slay???
